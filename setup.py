702b0a52706842e958bfda626299da20ea132e3e
Carl J. Nobile <carl.nobile@gmail.com>
2014-07-08 21:36:36 -0400

    Added packaging files.

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-dcolumns',
+    version='0.1',
+    packages=['dcolumn'],
+    include_package_data=True,
+    license='MIT License',
+    description=('An app to give any database model the ability to '
+                 'dynamilcly add fields.'),
+    long_description=README,
+    url='https://github.com/cnobile2012/dcolumn',
+    author='Carl J. Nobile',
+    author_email='carl.nobile@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
