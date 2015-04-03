779eb9843c738a1ccd76e2c93940e8024df63181
Carl J. Nobile <carl.nobile@gmail.com>
2015-04-02 23:14:14 -0400

    Updated the version number.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-dcolumns',
-    version='0.5.0',
+    version='0.5.1',
     packages=['dcolumn', 'dcolumn.dcolumns', 'dcolumn.common',],
     include_package_data=True,
     license='MIT',
