ee8524a74db2b1830e0286ed412685ccbc667b52
Carl J. Nobile <carl.nobile@gmail.com>
2016-02-28 19:53:56 -0500

    Added documentation.

--- dcolumn/dcolumns/urls.py
+++ dcolumn/dcolumns/urls.py
@@ -2,6 +2,14 @@
 # dcolumn/dcolumns/urls.py
 #
 
+"""
+URLs used in DColumns.
+
+url(r'api/collections/(?P<class_name>\w+)/$', collection_ajax_view,
+        name="api-collections")
+"""
+__docformat__ = "restructuredtext en"
+
 from django.conf.urls import *
 from .views import collection_ajax_view
 
