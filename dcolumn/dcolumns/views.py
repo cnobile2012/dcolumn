1bd4f14028bdb09dfac249513af23c1e45a81e87
Carl J. Nobile <carl.nobile@gmail.com>
2016-02-28 19:55:00 -0500

    Added documentation.

--- dcolumn/dcolumns/views.py
+++ dcolumn/dcolumns/views.py
@@ -19,10 +19,42 @@ log = logging.getLogger('dcolumns.dcolumns.views')
 
 
 class ContextDataMixin(object):
+    """
+    Mixin for context data.
+    """
 
-    def get_dynamic_column_context_data(self, class_name='', **kwargs):
+    def get_dynamic_column_context_data(self, class_name=''):
         """
-        
+        Generates the data needed for HTML select option tags. This data is
+        in a dict keyed by ``dynamicColumns`` giving it a namespace in the
+        context. The slug for each ``CHOICE`` object is the key to the list
+        used for the option tags.
+
+        Example of Output::
+
+          {'dynamicColumns': {
+           u'author': [(0, 'Choose a value'),
+                       (17, u'Jeremy Blum'),
+                       (5, u'John Iovine'),
+                       (23, u'Lauren Darcey & Shane Conder'),
+                       (25, u'Peter Gasston'),
+                       (6, u'Rajaram Regupathy'),
+                       (7, u'Richard Stones'),
+                       (15, u'Ruth Suehle & Tom Callaway'),
+                       (8, u'Toby Segaran')],
+           u'language': [(0, 'Choose a value'),
+                         (1, 'Chinese'),
+                         (2, 'English'),
+                         (3, 'Portuguese'),
+                         (4, 'Russian'),
+                         (5, 'Japanese')],
+           ...}
+          }
+
+        :param class_name: This is a mandatory keyword that defines the
+                           ``ColumnCollection`` name.
+        :type class_name: str
+        :rtype: dict
         """
         context = {}
         fk_slugs = DynamicColumn.objects.get_fk_slugs()
@@ -42,12 +74,48 @@ class ContextDataMixin(object):
         log.debug("context: %s", context)
         return context
 
-    def get_relation_context_data(self, class_name='', obj=None, **kwargs):
+    def get_relation_context_data(self, class_name='', obj=None, form=None):
         """
-        
-        """
-        form = kwargs.get('form')
+        Generates an OrderedDict of meta data needed to determine how the
+        values of a ``KeyValue`` is to interpreted. If ``obj`` is supplied
+        and ``form`` is not supplied values from the ``KeyWord`` objects will
+        be included in the list of meta data. If ``form`` is supplied the
+        meta data will be taken from the Django form exclusively.
 
+        Example of Output::
+
+          OrderedDict([
+            (19,
+              {'location': 'book-bottom',
+               'name': u'Promotion',
+               'order': 1,
+               'pk': 19,
+               'relation': 2,
+               'required': False,
+               'slug': u'promotion',
+               'store_relation': True,
+               'value_type': 2}),
+            (20,
+              {'location': 'book-center',
+               'name': u'Language',
+               'order': 6,
+               'pk': 20,
+               'relation': 1,
+               'required': False,
+               'slug': u'language',
+               'store_relation': False,
+               'value_type': 2}),
+            ...])
+
+        :param class_name: This is a mandatory keyword that defines the
+                           ``ColumnCollection`` name.
+        :type class_name: str
+        :param obj: Optional model object that inherits from ``CollectionBase``.
+        :type obj: object
+        :param form: Optional form object.
+        :type: Django Form object.
+        :rtype: OrderedDict
+        """
         if form:
             relations = form.get_display_data()
         else:
@@ -63,15 +131,22 @@ class ContextDataMixin(object):
 # CollectionAJAXView
 #
 class CollectionAJAXView(JSONResponseMixin, TemplateView, ContextDataMixin):
+    """
+    Web service endpoint used in the Django admin to format ``KeyValue`` values
+    as per the ``DynamicColumn`` meta data.
+    """
     http_method_names = ('get',)
 
     @method_decorator(dcolumn_login_required)
     def dispatch(self, *args, **kwargs):
+        """
+        Django view dispatch decorated for login requierments.
+        """
         return super(CollectionAJAXView, self).dispatch(*args, **kwargs)
 
     def get_context_data(self, **kwargs):
         """
-        Get context data for the KeyValue objects.
+        Get context data for the ``KeyValue`` objects.
 
         Do not call super on get_context_data, it puts self into the context
         which is not valid JSON.
@@ -84,7 +159,7 @@ class CollectionAJAXView(JSONResponseMixin, TemplateView, ContextDataMixin):
             context.update(self.get_relation_context_data(**kwargs))
         except Exception, e:
             context['valid'] = False
-            context['message'] = "Error occured: {}".format(e)
+            context['message'] = "Error occurred: {}".format(e)
             log.error(context['message'], exc_info=True)
 
         return context
@@ -96,13 +171,20 @@ collection_ajax_view = CollectionAJAXView.as_view()
 # CollectionCreateUpdateViewMixin
 #
 class CollectionCreateUpdateViewMixin(ContextDataMixin):
+    """
+    This mixin is needed by any create or update view where the view is
+    associated with a model that inherits ``CollectionBase``.
+    """
 
     def get_initial(self):
+        """
+        Provides initial data to forms.
+        """
         return {'request': self.request}
 
     def get_context_data(self, **kwargs):
         """
-        Get context data for the KeyValue objects.
+        Get context data for the ``KeyValue`` objects.
         """
         kwargs['class_name'] = self.model.__name__
         context = super(CollectionCreateUpdateViewMixin,
@@ -118,10 +200,14 @@ class CollectionCreateUpdateViewMixin(ContextDataMixin):
 # CollectionDetailViewMixin
 #
 class CollectionDetailViewMixin(ContextDataMixin):
+    """
+    This mixin is needed by any detail view where the view is associated with
+    a model that inherits ``CollectionBase``.
+    """
 
     def get_context_data(self, **kwargs):
         """
-        Get context data for the KeyValue objects.
+        Get context data for the ``KeyValue`` objects.
         """
         kwargs['class_name'] = self.model.__name__
         context = super(CollectionDetailViewMixin,
