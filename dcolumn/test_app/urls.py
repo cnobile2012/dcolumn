a512281f8ab477bdb25a65ab627d265d3a8ac066
Carl J. Nobile <carl.nobile@gmail.com>
2023-06-19 21:31:38 -0400

    Changed re_path tp path in a few places.

--- dcolumn/test_app/urls.py
+++ dcolumn/test_app/urls.py
@@ -1,22 +1,19 @@
 #
-# dcolumn/dcolumns/tests/urls.py
+# dcolumn/test_app/urls.py
 #
 
-try:
-    from django.urls import re_path
-except:
-    from django.conf.urls import url as re_path
+from django.urls import re_path, path
 
 from . import views
 
 
 urlpatterns = [
-    re_path(r'^test-book-create/$', views.test_book_create_view,
-        name='test-book-create'),
+    path('test-book-create/', views.test_book_create_view,
+         name='test-book-create'),
     re_path(r'^test-book-update/(?P<pk>\d+)/$', views.test_book_update_view,
         name='test-book-update'),
     re_path(r'^test-book-detail/(?P<pk>\d+)/$', views.test_book_detail_view,
         name='test-book-detail'),
-    re_path(r'^test-book-list/$', views.test_book_list_view,
-        name='test-book-list'),
+    path('test-book-list/', views.test_book_list_view,
+         name='test-book-list'),
     ]
