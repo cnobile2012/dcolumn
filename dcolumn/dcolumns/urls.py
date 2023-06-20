d0ed2c4138a122b384bcd3285a8959a2e25b23cf
Carl J. Nobile <carl.nobile@gmail.com>
2023-06-19 21:30:25 -0400

    Removed usless comment.

--- dcolumn/dcolumns/urls.py
+++ dcolumn/dcolumns/urls.py
@@ -4,14 +4,13 @@
 
 """
 URLs used in DColumns.
-
-re_path(r'api/collections/(?P<class_name>\w+)/$', collection_ajax_view,
-        name="api-collections")
 """
 __docformat__ = "restructuredtext en"
 
 from django.urls import include, re_path, path
+
 from .views import collection_ajax_view
+
 
 app_name = 'dcolumns'
 urlpatterns = [
